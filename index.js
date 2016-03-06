f19c50d624534ec25039baa7c06d973d0011d4ca
amyh576 <amy.ho576@gmail.com>
2016-03-06 05:15:15 +0000

    Add mongodb support

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var app = express();
 var bodyParser = require('body-parser');
+var Users = require('./models/users.js'); // models usually have uppercase variables
 
 // configure the app
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -9,12 +10,30 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', function (req, res) {
-    res.render('index');
+  Users.count(function (err, users) {
+    if (err) {
+      res.send('error getting users');
+    }
+    else {
+    res.render('index', {userCount: users.length});
+    }
+  });
 });
 
 app.post('/user/register', function (req, res) {
-    res.send(req.body);
-    console.log('The user has the email address', req.body.email);
+  var newUser = new Users();
+  newUser.hashed_password = req.body.password;
+  newUser.email = req.body.email;
+  newUser.name = req.body.fl_name;
+  newUser.save(function(err){
+    if (err) {
+      res.send('There was a problem saving the user');
+    }
+    else
+    {
+      res.redirect('/');
+    }
+  })
 });
 
 app.listen(process.env.PORT, function () {
