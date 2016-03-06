e820327afb504c676da96f92e18fe8c37ab2116c
amyh576 <amy.ho576@gmail.com>
2016-03-06 04:37:34 +0000

    Parse submitted forms

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var app = express();
+var bodyParser = require('body-parser');
 
+// configure the app
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', function (req, res) {
     res.render('index');
 });
 
-app.listen(8080, function () {
+app.post('/user/register', function (req, res) {
+    res.send(req.body);
+    console.log('The user has the email address', req.body.email);
+});
+
+app.listen(process.env.PORT, function () {
   console.log('Example app listening on port ' + process.env.PORT);
 });
